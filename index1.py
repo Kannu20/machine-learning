2cfee013ee98bcbb10a2f15d5b6ca88c5f167fda
Kanishak todwal <kannutodwal@gmail.com>
2026-08-24 13:05:52 +0530

    bar-graph

--- index1.py
+++ index1.py
@@ -17,3 +17,20 @@ print(df.shape)
 
 
 print(df.head())
+
+print(df.describe())
+
+print(df.isnull().sum())
+
+print(df.columns)
+
+numeric_columns = ['age', 'bmi', 'children', 'charges']
+
+for col in numeric_columns:
+    plt.figure(figsize=(6, 4))
+    sns.histplot(df[col], kde=True, bins=30) #kde=True for Kernel Density Estimation
+    plt.title(f'Distribution of {col}')
+    plt.xlabel(col)
+    plt.ylabel('Frequency')
+    plt.show()
+    
