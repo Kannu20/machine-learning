0d3669e0dcd84d74ac45c901e920281a2e2a5cc0
Kanishak todwal <kannutodwal@gmail.com>
2026-09-11 23:44:34 +0530

    update

---
+++ two_sample_t_test.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy import stats
+
+group_A = [85, 88, 90, 92, 87, 85, 89, 91, 86, 88]
+group_B = [82, 84, 80, 83, 81, 79, 78, 85, 84, 83]
+
+t_stats , p_value = stats.ttest_ind(group_A, group_B)
+
+print(f"T-Statistic: {t_stats}")
+
+alpha = 0.05
+
+if p_value < alpha:
+    print("Reject the null hypothesis")
+else:
+    print("Accept the null hypothesis")
