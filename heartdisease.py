0d932c547a5e064d2626f4398723e02e71868170
Kanishak todwal <kannutodwal@gmail.com>
2026-09-15 23:28:17 +0530

    update

---
+++ heartdisease.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings("ignore")
+df = pd.read_csv("heart.csv")
+print(df.head())
+
+print(df.head())
+df_encoded = pd.get_dummies(df,drop_first=True)
+print(df_encoded)
+
+df_encoded = df_encoded.astype(int)
+print(df_encoded.head())
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report, f1_score, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+X = df_encoded.drop('HeartDisease', axis=1)
+y = df_encoded['HeartDisease']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+models = {
+    "Logistic Regression": LogisticRegression(),
+    "KNN": KNeighborsClassifier(),
+    "Naive Bayes": GaussianNB(),
+    "Decision Tree": DecisionTreeClassifier(),
+    "SVM (RBF Kernel)": SVC(probability=True)
+}
+
+results = []
+
+for name, model in models.items():
+    model.fit(X_train_scaled, y_train)
+    y_pred = model.predict(X_test_scaled)
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    results.append({
+        'Model': name,
+        'Accuracy': round(acc, 4),
+        'F1 Score': round(f1, 4)
+    })
+
+print("Results:",results)
